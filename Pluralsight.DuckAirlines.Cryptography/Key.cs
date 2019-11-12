d5e410bf2073553c217e3e9604b3883efe7a32cd
Ed <ed@s.com>
2019-11-12 17:11:14 +0000

    Update

--- Pluralsight.DuckAirlines.Cryptography/Key.cs
+++ Pluralsight.DuckAirlines.Cryptography/Key.cs
@@ -22,7 +22,46 @@ namespace Pluralsight.DuckAirlines.Cryptography
 
         public static KeyConfiguration ConfigureKeyPair()
         {
-            
+            var keyConfiguration = new KeyConfiguration();
+
+            Console.WriteLine("\nCertificate Signing Request Certificate\n" +
+                              "---------------------------\n" +
+                              "This process will create a Public / Private key pair as well as \n" +
+                              "create a certificate signing request for the public key.\n" +
+                              "You are going to be walked through each piece of information\n" +
+                              "needed for the Certificate Signing Request.\n");
+            Console.Write("Country: ");
+            keyConfiguration.DistinguishedName.Country = Console.ReadLine();
+            Console.Write("State or Locality: ");
+            keyConfiguration.DistinguishedName.State = Console.ReadLine();
+            Console.Write("City: ");
+            keyConfiguration.DistinguishedName.Locality = Console.ReadLine();
+            Console.Write("Organization: ");
+            keyConfiguration.DistinguishedName.Organization = Console.ReadLine();
+            Console.Write("Organizational Unit: ");
+            keyConfiguration.DistinguishedName.OrganizationalUnit = Console.ReadLine();
+            Console.Write("Common Name: ");
+            keyConfiguration.DistinguishedName.CommonName = Console.ReadLine();
+            Console.Write("Email Address: ");
+            keyConfiguration.DistinguishedName.EmailAddress = Console.ReadLine();
+
+            keyConfiguration.KeystoreFileName =
+                keyConfiguration.DistinguishedName.CommonName?.Replace(" ", string.Empty) + ".key";
+            keyConfiguration.CertificateRequestFileName =
+                keyConfiguration.DistinguishedName.CommonName?.Replace(" ", string.Empty) + ".csr";
+
+            Console.Write("Private Key Password: ");
+            Console.ReadLine();
+
+            Console.Write($"\nKey Store FileName [{keyConfiguration.KeystoreFileName}]: ");
+            var tempFileName = Console.ReadLine();
+            if (!string.IsNullOrEmpty(tempFileName)) keyConfiguration.KeystoreFileName = tempFileName;
+
+            Console.Write($"CSR FileName [{keyConfiguration.CertificateRequestFileName}]: ");
+            tempFileName = Console.ReadLine();
+            if (!string.IsNullOrEmpty(tempFileName)) keyConfiguration.CertificateRequestFileName = tempFileName;
+
+            return keyConfiguration;
         }
     }
 }
